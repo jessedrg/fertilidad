f9357c27914e3d5259f755b44e83b8da80486f54
Jesse Dragstra <jesse.dragstracoll@dowjones.com>
2026-01-23 13:30:49 +0100

    Split sitemap into 5 smaller sitemaps for better indexing

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,12 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: ["/api/", "/admin/"],
     },
-    sitemap: "https://www.tufertilidad.xyz/sitemap-v1.xml",
+    sitemap: [
+      "https://www.tufertilidad.xyz/sitemap-1.xml",
+      "https://www.tufertilidad.xyz/sitemap-2.xml",
+      "https://www.tufertilidad.xyz/sitemap-3.xml",
+      "https://www.tufertilidad.xyz/sitemap-4.xml",
+      "https://www.tufertilidad.xyz/sitemap-5.xml",
+    ],
   }
 }
---
+++ app/sitemap-3.xml/route.ts
@@ -0,0 +1,49 @@
+import { NextResponse } from "next/server"
+import { VALID_SERVICES, MODIFIERS, PROBLEMS, CITIES } from "@/lib/sitemap-data"
+
+export const dynamic = "force-dynamic"
+export const runtime = "nodejs"
+
+const BASE_URL = "https://www.tufertilidad.xyz"
+const MAX_URLS_PER_SITEMAP = 45000
+
+// Sitemap 3: Services 5-6 (congelacion-ovulos, tratamiento-fertilidad)
+const SERVICES_CHUNK = VALID_SERVICES.slice(4, 6)
+
+export async function GET() {
+  const date = new Date().toISOString().split("T")[0]
+  const sitemaps: string[] = []
+
+  for (const service of SERVICES_CHUNK) {
+    for (const modifier of MODIFIERS) {
+      const id = modifier ? `${service}${modifier}` : service
+      sitemaps.push(`${BASE_URL}/sitemap-files/${id}.xml`)
+    }
+
+    const problems = PROBLEMS[service] || []
+    const urlsPerProblem = CITIES.length
+    const problemsPerChunk = Math.floor(MAX_URLS_PER_SITEMAP / urlsPerProblem)
+    const numChunks = Math.ceil(problems.length / problemsPerChunk)
+    
+    for (let i = 1; i <= numChunks; i++) {
+      sitemaps.push(`${BASE_URL}/sitemap-files/${service}-necesidad-${i}.xml`)
+    }
+  }
+
+  let xml = '<?xml version="1.0" encoding="UTF-8"?>\n'
+  xml += '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
+
+  for (const sitemap of sitemaps) {
+    xml += `  <sitemap>\n    <loc>${sitemap}</loc>\n    <lastmod>${date}</lastmod>\n  </sitemap>\n`
+  }
+
+  xml += "</sitemapindex>"
+
+  return new NextResponse(xml, {
+    status: 200,
+    headers: {
+      "Content-Type": "application/xml; charset=utf-8",
+      "Cache-Control": "public, max-age=86400, s-maxage=86400",
+    },
+  })
+}
